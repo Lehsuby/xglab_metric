45ab089c4abc6dd3c5ef755cb4a8fd424fb69d7d
DaniilGumin <gumin.danya@gmail.com>
2023-02-16 21:52:31 +0300

    feat: create module

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(name='xglab_metric',
+      version='0.1.0',
+      description='Abstractions library for 3rd party metrics development at xglab.pro',
+      url='https://github.com/xglab-pro/xglab_metric',
+      author='Egor Gumin',
+      author_email='',
+      license='Unlicensed',
+      packages=find_packages(),
+      install_requires=[],
+      zip_safe=False,
+      include_package_data=True
+      )
