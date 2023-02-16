4361e61b9177f59ba3c227e4b98657abebf72801
Egor Gumin <gumin.egor@gmail.com>
2023-02-16 23:48:30 +0400

    Update imports

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from xglab_metric.Event import *
-from xglab_metric.enums import *
-from xglab_metric.NumericMetric import *
-from xglab_metric.ValuedEvent import *
---
+++ xglab_metric/__init__.py
@@ -0,0 +1,4 @@
+from xglab_metric.Event import Event
+from xglab_metric.enums import *
+from xglab_metric.NumericMetric import NumericMetric
+from xglab_metric.ValuedEvent import ValuedEvent
