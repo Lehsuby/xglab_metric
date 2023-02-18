547f3d8cce85c25383c876b7c825544e7c4265b9
Egor Gumin <gumin.egor@gmail.com>
2023-02-18 20:59:26 +0400

    Add TotalProbability to selection

--- xglab_metric/team_value/strategy_selector.py
+++ xglab_metric/team_value/strategy_selector.py
@@ -1,11 +1,14 @@
 from typing import Dict, List
 
 from xglab_metric.ValuedEvent import ValuedEvent
-from xglab_metric.team_value.TeamValueStrategyType import TeamValueStrategyType
 from xglab_metric.team_value.KeepValue import KeepValue
+from xglab_metric.team_value.TeamValueStrategyType import TeamValueStrategyType
+from xglab_metric.team_value.TotalProbability import TotalProbability
 
 
 def get_team_value_map(strategy: TeamValueStrategyType, valued_events: List[ValuedEvent]) -> Dict[str, float]:
     if strategy == TeamValueStrategyType.KeepValue:
         return KeepValue.get_team_value_map(valued_events)
+    if strategy == TeamValueStrategyType.TotalProbability:
+        return TotalProbability.get_team_value_map(valued_events)
     raise Exception(f'Unknown strategy {strategy}')
