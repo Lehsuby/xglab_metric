dce364c92d313f0a99bfff6addb3361c851d9600
Egor Gumin <gumin.egor@gmail.com>
2023-02-18 13:05:58 +0400

    Remove strategy_selector from outer import

--- xglab_metric/__init__.py
+++ xglab_metric/__init__.py
@@ -3,4 +3,3 @@ from xglab_metric.NumericMetric import NumericMetric
 from xglab_metric.ValuedEvent import ValuedEvent
 from xglab_metric.enums import *
 from xglab_metric.team_value import TeamValueStrategyType
-from xglab_metric.team_value import strategy_selector
