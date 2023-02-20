eca807d8d465a0096fbcdd4027d026e8e44540af
Egor Gumin <gumin.egor@gmail.com>
2023-02-20 20:47:10 +0400

    v1.0.1

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(name='xglab_metric',
-      version='1.0.0',
+      version='1.0.1',
       description='Abstractions library for 3rd party metrics development at xglab.pro',
       url='https://github.com/xglab-pro/xglab_metric',
       author='Egor Gumin',
       author_email='',
       license='Unlicensed',
       packages=find_packages(),
-      install_requires=[],
+      install_requires=['StrEnum==0.4.9'],
       zip_safe=False,
       include_package_data=True
       )
